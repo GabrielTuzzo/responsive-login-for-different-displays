6890bcd41b6ebd6dbd84caed1fd708aba19fb582
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2022-08-22 14:12:39 +0100

    new commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const button = document.getElementById('btn')
+const eyeElement = document.querySelector('.fa-eye')
 
 button.addEventListener('click', mostrarOcultarSenha)
 
@@ -6,7 +7,9 @@ function mostrarOcultarSenha () {
     const senha = document.getElementById("password")
     if (senha.type == 'password') {
         senha.type = 'text'
+        eyeElement.classList.add('fa-eye-slash')
     } else {
         senha.type = 'password'
+        eyeElement.classList.remove('fa-eye-slash')
     }
 }

acd08d3389a4e602b9dd328a3170e7d49e0f9c4e
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2023-02-04 18:38:45 +0000

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const button = document.getElementById('btn')
 const eyeElement = document.querySelector('.fa-eye')
 const passwordInput = document.querySelector('#password')
-const testeInput = document.querySelector('#teste')
+const usernameInput = document.querySelector('#username')
+const capslockInputpassword = document.querySelector('#capsLockinpassword')
+const capslockInputusername = document.querySelector('#capsLockinname')
 const name = document.getElementById('name')
 const form = document.getElementById('form')
 
@@ -19,8 +21,28 @@ function mostrarOcultarSenha () {
     }
 }
 
+usernameInput.addEventListener('keydown', (e) => {
+    const capslockOn = e.getModifierState('CapsLock')
+    capslockInputusername.style.display = capslockOn ? 'block' : 'none'
+})
+usernameInput.addEventListener('blur', () => {
+    capslockInputusername.style.display = 'none'
+})
+usernameInput.addEventListener('click', (e) => {
+    const capslockOn = e.getModifierState('CapsLock')
+    capslockInputusername.style.display = capslockOn ? 'block' : 'none'
+})
+
 passwordInput.addEventListener('keydown', (e) => {
-    const capsLockIn = e.getModifierState('CapsLock')
-    testeInput.style.display = capsLockIn ? 'block' : 'none'
-    
-    })
+    const capslockOn = e.getModifierState('CapsLock')
+    capslockInputpassword.style.display = capslockOn ? 'block' : 'none'
+})
+passwordInput.addEventListener('blur', () => {
+    capslockInputpassword.style.display = 'none'
+})
+passwordInput.addEventListener('click', (e) => {
+    const capslockOn = e.getModifierState('CapsLock')
+    capslockInputpassword.style.display = capslockOn ? 'block' : 'none'
+})
+
+
